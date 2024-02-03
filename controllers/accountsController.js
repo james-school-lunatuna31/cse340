c156989644f5f30c75371f096e4b0edb156ee66b
James Green <github.byui.school@ckplace.org>
2024-02-02 21:45:46 -0500

    team act done

--- controllers/accountsController.js
+++ controllers/accountsController.js
@@ -1,5 +1,6 @@
 const utilities = require("../utilities")
 const accountModel = require("../models/account-model.js")
+const bcrypt = require("bcryptjs")
 
 /* ****************************************
 *  Deliver login view
@@ -28,12 +29,24 @@ async function buildLogin(req, res, next) {
 async function registerAccount(req, res) {
     let nav = await utilities.getNav()
     const { account_firstname, account_lastname, account_email, account_password } = req.body
-
+// Hash the password before storing
+let hashedPassword
+try {
+  // regular password and cost (salt is generated automatically)
+  hashedPassword = await bcrypt.hash(account_password, Math.floor(Math.random() * (14 - 11 + 1)) + 11)
+} catch (error) {
+  req.flash("notice", 'Sorry, there was an error processing the registration.')
+  res.status(500).render("account/register", {
+    title: "Registration",
+    nav,
+    errors: null,
+  })
+}
     const regResult = await accountModel.registerAccount(
       account_firstname,
       account_lastname,
       account_email,
-      account_password
+      hashedPassword
     )
   
     if (regResult) {
